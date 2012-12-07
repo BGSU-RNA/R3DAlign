728e6fdb660cef128e65b0a18d1037cafd5badc2
Anton Petrov <anton.i.petrov@gmail.com>
2012-12-06 19:09:31 -0500

    Pass extra parameter with filename, disable Excel on macs

--- R3DAlign/rWriteAlignmentFasta.m
+++ R3DAlign/rWriteAlignmentFasta.m
@@ -1,20 +1,19 @@
-function [] = rWriteAlignmentFasta(File1,NTIndices1,File2,NTIndices2,AlignedNTIndices1,AlignedNTIndices2,NTList)
+function [] = rWriteAlignmentFasta(File1,NTIndices1,File2,NTIndices2,AlignedNTIndices1,AlignedNTIndices2,NTList,filename)
+
+if nargin < 8
+    filename = 'Alignment'; 
+end
 
 if ischar(File1),
   Filename = File1;
-  File1 = zGetNTData(Filename,0);
+  File1 = zAddNTData(Filename);
 end
 if ischar(File2),
   Filename = File2;
-  File2 = zGetNTData(Filename,0);
+  File2 = zAddNTData(Filename);
 end
 
-File(1)=File1;
-File(2)=File2;
-
-date = regexprep(datestr(now),':', '-');
-FastaName=['R3D Align ' File1.Filename ' ' File2.Filename ' ' date(1:17) '.fasta'];
-
+FastaName=[filename '.fasta'];
 fidOUT = fopen(FastaName,'w+');
 
 numAl=length(AlignedNTIndices1);
@@ -47,21 +46,33 @@ end
 
 L = length(Alignment);
 for k=1:2,
-  if ischar(NTList{k}) && ~isequal(lower(NTList{k}),'all')
-     fprintf(fidOUT,'> %s %s\r\n',File(k).Filename,NTList{k});
-  elseif iscell(NTList{k})
-     fprintf(fidOUT,'> %s %s\r\n',File(k).Filename,NTList{k}{1});
+  if k==1
+    File=File1;
   else
-     fprintf(fidOUT,'> %s\r\n',File(k).Filename);
+    File=File2;
   end
-  C = 1;
+%  if ischar(NTList{k}) && ~isequal(lower(NTList{k}),'all')
+%     fprintf(fidOUT,'> %s %s\r\n',File.Filename,NTList{k});
+%  elseif iscell(NTList{k})
+%     fprintf(fidOUT,'> %s %s\r\n',File.Filename,NTList{k}{1});
+%  else
+%     fprintf(fidOUT,'> %s\r\n',File.Filename);
+%  end
+ 
+% The above was commented out and replaced with the line below because the 
+% nt numbers were being written as 200,201,202... instead of 200:300 (for example).
+% To correct this, the text entered would have to be saved before being parsed.
+
+ fprintf(fidOUT,'> %s\r\n',File.Filename);
+
+ C = 1;
   while C <= L
     c=1;
     while c<=80 && C<=L
        if Alignment(C,k)==0
           fprintf(fidOUT,'-');
        else
-          fprintf(fidOUT,'%s', File(k).NT(Alignment(C,k)).Base);
+          fprintf(fidOUT,'%s', File.NT(Alignment(C,k)).Base);
        end
        c = c + 1;
        C = C + 1;
@@ -69,5 +80,5 @@ for k=1:2,
     fprintf(fidOUT,'\r\n');
   end
 end
-
+    
 fclose(fidOUT);
