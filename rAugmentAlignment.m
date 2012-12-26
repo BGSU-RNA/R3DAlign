0e4c00cde57afbd21f0f934036e575186887a076
Craig L. Zirbel <zirbel@bgnet.bgsu.edu>
2012-12-25 22:43:47 -0500

    Changed indentation, thought I would need to change more.

--- rAugmentAlignment.m
+++ rAugmentAlignment.m
@@ -4,28 +4,28 @@
 
 function [align1, align2] = rAugmentAlignment(Indices1,align1,align2)
 
-   for i = Indices1,
-      if ~any(align1==i)
-         align1 = [align1 i]; %#ok<AGROW>
-         align2 = [align2 0]; %#ok<AGROW>
-      end
-   end
-   [S1 IX] = sort(align1);
-   align1 = S1; %#ok<NASGU>
-   align2 = align2(IX);
+for i = Indices1,
+  if ~any(align1==i)
+    align1 = [align1 i]; %#ok<AGROW>
+    align2 = [align2 0]; %#ok<AGROW>
+  end
+end
+[S1 IX] = sort(align1);
+align1 = S1; %#ok<NASGU>
+align2 = align2(IX);
 
-   GappedAs=find(align2==0);
-   for p=GappedAs
-      tmp = 0;
-      ct = 0;
-      while tmp == 0
-         if tmp == 0
-            tmp = align2(max(p-ct,1));
-         end
-         if tmp == 0
-            tmp = align2(min(p+ct,length(align2)));
-         end
-         ct=ct+1;
-      end
-      align2(p)=tmp;
-   end
+GappedAs=find(align2==0);
+for p=GappedAs
+  tmp = 0;
+  ct = 0;
+  while tmp == 0
+    if tmp == 0
+      tmp = align2(max(p-ct,1));
+    end
+    if tmp == 0
+      tmp = align2(min(p+ct,length(align2)));
+    end
+    ct=ct+1;
+  end
+  align2(p)=tmp;
+end
